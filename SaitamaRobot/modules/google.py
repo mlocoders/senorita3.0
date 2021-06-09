a66219e898e0527ce350bbabfcb5163cd5cc69ed
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-06-10 01:22:42 +0530

    Create google.py

---
+++ SaitamaRobot/modules/google.py
@@ -0,0 +1,90 @@
+from bs4 import BeautifulSoup
+import urllib
+from MashaRoBot import telethn as tbot
+import glob
+import io
+import os
+import re
+import aiohttp
+import urllib.request
+from urllib.parse import urlencode
+import requests
+from bs4 import BeautifulSoup
+from PIL import Image
+from search_engine_parser import GoogleSearch
+
+import bs4
+import html2text
+from bing_image_downloader import downloader
+from telethon import *
+from telethon.tl import functions
+from telethon.tl import types
+from telethon.tl.types import *
+
+from SaitamaRobot import *
+
+from SaitamaRobot.events import register
+
+opener = urllib.request.build_opener()
+useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36"
+opener.addheaders = [("User-agent", useragent)]
+
+
+@register(pattern="^/google (.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    
+    webevent = await event.reply("searching........")
+    match = event.pattern_match.group(1)
+    page = re.findall(r"page=\d+", match)
+    try:
+        page = page[0]
+        page = page.replace("page=", "")
+        match = match.replace("page=" + page[0], "")
+    except IndexError:
+        page = 1
+    search_args = (str(match), int(page))
+    gsearch = GoogleSearch()
+    gresults = await gsearch.async_search(*search_args)
+    msg = ""
+    for i in range(len(gresults["links"])):
+        try:
+            title = gresults["titles"][i]
+            link = gresults["links"][i]
+            desc = gresults["descriptions"][i]
+            msg += f"❍[{title}]({link})\n**{desc}**\n\n"
+        except IndexError:
+            break
+    await webevent.edit(
+        "**Search Query:**\n`" + match + "`\n\n**Results:**\n" + msg, link_preview=False
+    )
+
+@register(pattern="^/img (.*)")
+async def img_sampler(event):
+    if event.fwd_from:
+        return
+    
+    query = event.pattern_match.group(1)
+    jit = f'"{query}"'
+    downloader.download(
+        jit,
+        limit=4,
+        output_dir="store",
+        adult_filter_off=False,
+        force_replace=False,
+        timeout=60,
+    )
+    os.chdir(f'./store/"{query}"')
+    types = ("*.png", "*.jpeg", "*.jpg")  # the tuple of file types
+    files_grabbed = []
+    for files in types:
+        files_grabbed.extend(glob.glob(files))
+    await tbot.send_file(event.chat_id, files_grabbed, reply_to=event.id)
+    os.chdir("/app")
+    os.system("rm -rf store")
+
+
+opener = urllib.request.build_opener()
+useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36"
+opener.addheaders = [("User-agent", useragent)]
