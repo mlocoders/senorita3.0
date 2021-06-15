f1b924770f106f7c68cd49a68e8d0432e4921511
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-06-15 13:02:34 +0530

    Update stickers.py

--- SaitamaRobot/modules/stickers.py
+++ SaitamaRobot/modules/stickers.py
@@ -591,6 +591,7 @@ __help__ = """
  • `/stickerid` : reply to a sticker to me to tell you its file ID.
  • `/getsticker` : reply to a sticker to me to upload its raw PNG file.
  • `/kang` : reply to a sticker to add it to your pack.
+ • `/mmf` : memefiy any sticker and image.
  • `/stickers` : Find stickers for given term on combot sticker catalogue
 """
 
