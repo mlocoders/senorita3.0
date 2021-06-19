5dc0f444052a083a4fa3fc13ae7d93fd38cba0fc
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-06-19 21:12:49 +0530

    Update chatbot_mongo.py

--- SaitamaRobot/modules/mongo/chatbot_mongo.py
+++ SaitamaRobot/modules/mongo/chatbot_mongo.py
@@ -1,28 +1,28 @@
 from SaitamaRobot import mongodb as db_x
 
-Eren = db_x["CHATBOT"]
+eren = db_x["CHATBOT"]
 
 
 def add_chat(chat_id):
-    stark = Eren.find_one({"chat_id": chat_id})
+    stark = eren.find_one({"chat_id": chat_id})
     if stark:
         return False
     else:
-        Eren.insert_one({"chat_id": chat_id})
+        eren.insert_one({"chat_id": chat_id})
         return True
 
 
 def remove_chat(chat_id):
-    stark = Eren.find_one({"chat_id": chat_id})
+    stark = eren.find_one({"chat_id": chat_id})
     if not stark:
         return False
     else:
-        Eren.delete_one({"chat_id": chat_id})
+        eren.delete_one({"chat_id": chat_id})
         return True
 
 
 def get_all_chats():
-    r = list(Eren.find())
+    r = list(eren.find())
     if r:
         return r
     else:
@@ -30,7 +30,7 @@ def get_all_chats():
 
 
 def get_session(chat_id):
-    stark = Eren.find_one({"chat_id": chat_id})
+    stark = eren.find_one({"chat_id": chat_id})
     if not stark:
         return False
     else:
