51c931ff69e96465e54d5caf7eb29c6633b5de7a
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-06-23 04:43:59 +0530

    Create consts.py

---
+++ SaitamaRobot/utils/consts.py
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2018-2021 Amano Team
+
+from typing import List
+
+import httpx
+
+group_types: List[str] = ("group", "supergroup")
+
+admin_status: List[str] = ["creator", "administrator"]
+
+timeout = httpx.Timeout(40, pool=None)
+
+http = httpx.AsyncClient(http2=True, timeout=timeout)
+
+
+class Permissions:
+    can_be_edited: str = "can_be_edited"
+    delete_messages: str = "can_delete_messages"
+    restrict_members: str = "can_restrict_members"
+    promote_members: str = "can_promote_members"
+    change_info: str = "can_change_info"
+    invite_users: str = "can_invite_users"
+    pin_messages: str = "can_pin_messages"
