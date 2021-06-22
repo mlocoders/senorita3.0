763f7c71d608fd4934f49bf1b0646f936bc1c0d6
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-06-23 04:28:09 +0530

    Update forcesubscribe.py

--- SaitamaRobot/modules/forcesubscribe.py
+++ SaitamaRobot/modules/forcesubscribe.py
@@ -158,7 +158,7 @@ def _check_member(client, message):
 @pbot.on_message(filters.command(["forcesubscribe", "forcesub"]) & ~filters.private)
 def config(client, message):
     user = client.get_chat_member(message.chat.id, message.from_user.id)
-    if user.status is "creator" or user.user.id == 1141839926:
+    if user.status is "creator":
         chat_id = message.chat.id
         if len(message.command) > 1:
             input_str = message.command[1]
