53ce98b60f31ad4e20a7adb9fe97677b4b7b404e
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-06-23 02:42:15 +0530

    Update locks.py

--- SaitamaRobot/modules/locks.py
+++ SaitamaRobot/modules/locks.py
@@ -648,4 +648,4 @@ dispatcher.add_handler(LOCKTYPES_HANDLER)
 dispatcher.add_handler(LOCKED_HANDLER)
 
 dispatcher.add_handler(
-    MessageHandler(Filters.all & Filters.chat_type.groups, del_lockables), PERM_GROUP)
+    MessageHandler(Filters.all & Filters.chat_type.groups, del_lockables, run_async=True), PERM_GROUP)
