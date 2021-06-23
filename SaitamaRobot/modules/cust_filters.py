871b5c4d2d67982c5eee32805cc8eddcb96fa22f
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-06-23 13:28:49 +0530

    Update cust_filters.py

--- SaitamaRobot/modules/cust_filters.py
+++ SaitamaRobot/modules/cust_filters.py
@@ -393,9 +393,7 @@ def reply_filter(update, context):
                     ENUM_FUNC_MAP[filt.file_type](
                         chat.id,
                         filt.file_id,                        
-                        reply_to_message_id=message.message_id,
-                        parse_mode=ParseMode.HTML,
-                        disable_web_page_preview=True,
+                        reply_to_message_id=message.message_id,                        
                         reply_markup=keyboard,
                     )
                 break
