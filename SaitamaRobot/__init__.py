05bec629eed05e31c6369fee7e4c2ad6d754589b
MLO CODERS <86100976+mlocoders@users.noreply.github.com>
2021-10-04 20:41:57 +0530

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -216,10 +216,7 @@ WOLVES = list(WOLVES)
 DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
-# Load at end to ensure all prev variables have been set
-from SungJinwooRobot.modules.helper_funcs.handlers import (CustomCommandHandler,
-                                                        CustomMessageHandler,
-                                                        CustomRegexHandler)
+ 
 
 # make sure the regex handler can take extra kwargs
 tg.RegexHandler = CustomRegexHandler
